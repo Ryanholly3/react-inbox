83e470228e9d271231b2bcc9ecdb379e084928a0
ryanholly3 <ryanholly3@gmail.com>
2018-10-10 11:25:17 -0600

    added basic layout of page

---
+++ src/MessageList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MessageList extends React.Component {
+  render() {
+    return (
+      <div className="messageList">
+        messageList
+      </div>
+    );
+  }
+}
+
+export default MessageList;
